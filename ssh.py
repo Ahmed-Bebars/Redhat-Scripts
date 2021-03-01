d1334c66fd0685171246dfda67679e4691749f08
Ahmed Bebars <76910087+Ahmed-Bebars@users.noreply.github.com>
2021-03-01 09:08:05 +0200

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -8,11 +8,13 @@ import paramiko
 import sys
 import datetime
 import os
+#define needed variable
 result_file = open("/home/omc/bebars/result.txt", "w")
 result_file.close()
 currentDT = datetime.datetime.now().strftime('%Y-%m-%d')
 result_file = open("/home/omc/bebars/result.txt", "a")
-result_file.write (currentDT+"\n")
+result_file.write (currentDT+"\n");
+#ssh function
 def func_connect(ip,hostname,user,password):
 	result_file.write (hostname+"\n")
 	ssh = paramiko.SSHClient()
@@ -28,7 +30,7 @@ def func_connect(ip,hostname,user,password):
 	for line in stdout.readlines():
         	result_file.write (line.rstrip()+"\n")
 	ssh.close()
-
+#call ssh function
 func_connect("10.58.94.10","narvm5","root","Nokia!123")
 func_connect("10.58.95.205","nar2vm5","root","Nokia_123")
 result_file.close()
